86688cb294dab9634e426b6d5165060fd55b1416
Walteria9854 <yukikatayamaaapu@gmail.com>
2025-04-04 07:53:34 +0800

    Section6 Loop

--- Section5 MethodOverloading/src/Main.java
+++ Section5 MethodOverloading/src/Main.java
@@ -2,11 +2,8 @@ public class Main {
 
   public static void main(String[] args){
 
-    int newScore = calculateScore("Tim", 500);
-    System.out.println("New score is " + newScore);
-
-    calculateScore(75);
-    calculateScore();
+  System.out.println("New score is " + calculateScore("Tim", 500));
+  System.out.println("New score is " + calculateScore(10));
 
   }
 
@@ -16,12 +13,12 @@ public class Main {
   }
 
   public static int calculateScore(int score) {
-    System.out.println("Unnamed player scored " + score + "points");
-    return score * 1000;
+    return calculateScore("Anonymous", score);
   }
 
     public static int calculateScore(){
       System.out.println( "No player Name, No player score.");
       return 0;
-  }
+    }
+
 }
