42e2a2d29f945302b47135a2c95a0f342d0ffe16
Walteria9854 <yukikatayamaaapu@gmail.com>
2025-08-20 07:29:16 +0800

    Session8 smart kitchen challenge

--- Session7/Session7 StringFormatting/src/Main.java
+++ Session7/Session7 StringFormatting/src/Main.java
@@ -8,8 +8,26 @@ public class Main {
 
         String textBlock = """
                 Print a Bulleted List:
-                \u2022 First Point
-                \u2022 Sub Point""";
+                        \u2022 First Point
+                            \u2022 Sub Point""";
         System.out.println(textBlock);
+
+        int age = 35;
+        System.out.printf("Your age is %d%n", age);
+
+        int yearOfBirth = 2023 - age ;
+        System.out.printf("Age = %d, Birth year = %d%n", age, yearOfBirth);
+
+        System.out.printf("Your age is %f%n", (float)age);
+
+        for(int i = 1; i <= 10000; i *= 10) {
+            System.out.printf("Printing %20d %n", i);
+        }
+
+        String formattedString = String.format("Your age is %d", age);
+        System.out.println(formattedString);
+
+        formattedString = "Your age is %d".formatted(age);
+        System.out.println(formattedString);
     }
 }
