4ff98d97b84d510a49d9a3ebe4b49edad3f4e015
Walteria9854 <yukikatayamaapu@gmail.com>
2026-02-10 10:19:15 +0800

    updates extestion

--- Session12/Session12 GenericsExtra/src/dev/lpa/util/QueryList.java
+++ Session12/Session12 GenericsExtra/src/dev/lpa/util/QueryList.java
@@ -1,9 +1,11 @@
 package dev.lpa.util;
 
+import dev.lpa.model.Student;
 import java.util.ArrayList;
 import java.util.List;
+import javax.management.Query;
 
-public class QueryList <T extends QueryItem>{
+public class QueryList <T extends Student & QueryItem>{
 
   private List<T> items;
 
@@ -11,8 +13,8 @@ public class QueryList <T extends QueryItem>{
     this.items = items;
   }
 
-  public static <T extends QueryItem> List<T> getMatche (List<T> items, String field, String value ){
-    List<T> matches = new ArrayList<>();
+  public static <S extends QueryItem> List<S> getMatche (List<S> items, String field, String value ){
+    List<S> matches = new ArrayList<>();
     for(var item: items) {
       if (item.matchFieldValue(field, value)) {
         matches.add(item);
