1af687979d63b00ea3c0425f541577ad80602ad1
Walteria9854 <yukikatayamaaapu@gmail.com>
2026-04-28 16:29:21 +0800

    deck standard information

--- Session15/CollectionsMethods/src/dev/lpa/Card.java
+++ Session15/CollectionsMethods/src/dev/lpa/Card.java
@@ -1,15 +1,84 @@
 package dev.lpa;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public record Card(Suit suit, String face, int rank) {
 
   public enum Suit {
     CLUB, DIAMOND, HEART, SPADE;
 
     public char getImage(){
+
       return (new char[]{9827, 9830, 9829, 9824})[this.ordinal()];
     }
   }
 
-  
+  @Override
+  public String toString() {
+
+    int index = face.equals("10") ? 2 : 1;
+    String faceString = face.substring(0, index);
+    return "%s%c(%d)".formatted(faceString, suit.getImage(), rank);
+  }
+
+  public static Card getNumbericCard(Suit suit, int cardNumber) {
+
+    if(cardNumber > 1 && cardNumber < 11){
+      return new Card(suit, String.valueOf(cardNumber), cardNumber - 2);
+
+    }
+
+    System.out.println("Invalid Numveric card selected");
+    return null;
+
+  }
+
+  public static Card getFaceCard(Suit suit, char abbrev){
+    int charIndex = "JQKA".indexOf(abbrev);
+    if(charIndex > -1){
+      return new Card(suit, "" + abbrev, charIndex + 9);
+    }
+
+    System.out.println("Invalid Face card Selected");
+    return null;
+  }
+
+  public static List<Card> getStandardDeck(){
+    List<Card> deck = new ArrayList<>(52);
+    for(Suit suit : Suit.values()){
+      for(int i = 2; i < 10; i++){
+        deck.add(getNumbericCard(suit, i));
+      }
+
+    for(char c : new char[] {'J', 'Q', 'K', 'A'}){
+        deck.add(getFaceCard(suit, c));
+      }
+    }
+    return deck;
+  }
+
+
+  public static void printDeck(List<Card> deck){
+    printDeck(deck, "Current Deck", 4);
+  }
+
+  public static void printDeck(List<Card> deck, String desciption, int rows){
+
+    System.out.println("---------------------------------");
+    if(desciption != null){
+      System.out.println(desciption);
+
+    }
+
+    int cardsInRow = deck.size() / rows;
+    for(int i = 0; i < rows;i++){
+      int startIndex = i + cardsInRow;
+      int endIndex = startIndex + cardsInRow;
+      deck.subList(startIndex, endIndex).forEach(c -> System.out.print(c + " "));
+      System.out.println();
+    }
+  }
 
 }
