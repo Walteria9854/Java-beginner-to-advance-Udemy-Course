018d156af2b9fbcada3d5bbaea08972b391fb9f6
Walteria9854 <yukikatayamaaapu@gmail.com>
2026-05-14 15:57:36 +0800

    challenge with Session15 cards

---
+++ Session15/CollectionsMethods/src/dev/lpa/games/GameController.java
@@ -0,0 +1,22 @@
+package dev.lpa.games;
+
+import dev.lpa.Card;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class GameController {
+
+    public static void main(String[] args){
+
+        List<Card> createDeck = new ArrayList<>(52);
+
+
+
+    }
+
+
+
+
+}
